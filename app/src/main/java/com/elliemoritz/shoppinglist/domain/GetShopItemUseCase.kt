b746ae391d98dab28aa62793c072d189ac7022cc
Elisa Moritz <dark.little.finch@gmail.com>
2024-04-03 04:38:32 +0700

    feat: useCases methods implementations

--- app/src/main/java/com/elliemoritz/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/elliemoritz/shoppinglist/domain/GetShopItemUseCase.kt
@@ -1,8 +1,8 @@
 package com.elliemoritz.shoppinglist.domain
 
-class GetShopItemUseCase {
+class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
     fun getShopItem(id: Int): ShopItem {
-        TODO()
+        return shopListRepository.getShopItem(id)
     }
 }
