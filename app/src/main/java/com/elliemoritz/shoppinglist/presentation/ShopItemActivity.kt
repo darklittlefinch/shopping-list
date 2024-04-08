92fcaace63f1a394533c05311095efa1c5551ee7
Elisa Moritz <dark.little.finch@gmail.com>
2024-04-09 05:38:46 +0700

    fix: bug with screen rotation

--- app/src/main/java/com/elliemoritz/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/elliemoritz/shoppinglist/presentation/ShopItemActivity.kt
@@ -2,18 +2,14 @@ package com.elliemoritz.shoppinglist.presentation
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
 import com.elliemoritz.shoppinglist.R
 import com.elliemoritz.shoppinglist.databinding.ActivityShopItemBinding
 import com.elliemoritz.shoppinglist.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: ShopItemViewModel
     private lateinit var binding: ActivityShopItemBinding
 
     private var shopItemId: Int = ShopItem.UNDEFINED_ID
@@ -21,24 +17,11 @@ class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
-        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-
-        observeViewModel()
         setShopItemId()
-
-        if (shopItemId != ShopItem.UNDEFINED_ID) {
-            viewModel.getShopItem(shopItemId)
-        }
-
-        binding.buttonSave.setOnClickListener {
-            val name = binding.etName.text?.toString()
-            val count = binding.etCount.text?.toString()
-            if (shopItemId == ShopItem.UNDEFINED_ID) {
-                viewModel.addShopItem(name, count)
-            } else {
-                viewModel.editShopItem(shopItemId, name, count)
-            }
-        }
+        val fragment = ShopItemFragment.newInstance(shopItemId)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.shopItemContainer, fragment)
+            .commit()
     }
 
     private fun setupBinding() {
@@ -49,63 +32,6 @@ class ShopItemActivity : AppCompatActivity() {
 
     private fun setShopItemId() {
         shopItemId = intent.getIntExtra(EXTRA_ID, ShopItem.UNDEFINED_ID)
-    }
-
-    private fun observeViewModel() {
-        viewModel.shopItem.observe(this) {
-            binding.etName.setText(it.name)
-            binding.etCount.setText(it.count.toString())
-        }
-
-        viewModel.errorInputName.observe(this) {
-            binding.tilName.error = if (it) {
-                getString(R.string.error_incorrect_name)
-            } else {
-                null
-            }
-        }
-        setupResetErrorInputName()
-
-        viewModel.errorInputCount.observe(this) {
-            binding.tilCount.error = if (it) {
-                getString(R.string.error_incorrect_count)
-            } else {
-                null
-            }
-        }
-        setupResetErrorInputCount()
-
-        viewModel.shouldCloseScreen.observe(this) {
-            finish()
-        }
-    }
-
-    private fun setupResetErrorInputName() {
-        binding.etName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.resetErrorInputName()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
-    }
-
-    private fun setupResetErrorInputCount() {
-        binding.etCount.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.resetErrorInputCount()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
     }
 
     companion object {
---
+++ app/src/main/java/com/elliemoritz/shoppinglist/presentation/ShopItemFragment.kt
@@ -0,0 +1,135 @@
+package com.elliemoritz.shoppinglist.presentation
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.elliemoritz.shoppinglist.R
+import com.elliemoritz.shoppinglist.databinding.FragmentShopItemBinding
+import com.elliemoritz.shoppinglist.domain.ShopItem
+
+class ShopItemFragment : Fragment() {
+    private var _binding: FragmentShopItemBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private var shopItemId = ShopItem.UNDEFINED_ID
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setShopItemId()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentShopItemBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        observeViewModel()
+
+        if (shopItemId != ShopItem.UNDEFINED_ID) {
+            viewModel.getShopItem(shopItemId)
+        }
+
+        binding.buttonSave.setOnClickListener {
+            val name = binding.etName.text?.toString()
+            val count = binding.etCount.text?.toString()
+            if (shopItemId == ShopItem.UNDEFINED_ID) {
+                viewModel.addShopItem(name, count)
+            } else {
+                viewModel.editShopItem(shopItemId, name, count)
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.shopItem.observe(viewLifecycleOwner) {
+            binding.etName.setText(it.name)
+            binding.etCount.setText(it.count.toString())
+        }
+
+        viewModel.errorInputName.observe(viewLifecycleOwner) {
+            binding.tilName.error = if (it) {
+                getString(R.string.error_incorrect_name)
+            } else {
+                null
+            }
+        }
+        setupResetErrorInputName()
+
+        viewModel.errorInputCount.observe(viewLifecycleOwner) {
+            binding.tilCount.error = if (it) {
+                getString(R.string.error_incorrect_count)
+            } else {
+                null
+            }
+        }
+        setupResetErrorInputCount()
+
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            activity?.onBackPressedDispatcher?.onBackPressed()
+        }
+    }
+
+    private fun setShopItemId() {
+        val args = requireArguments()
+        if (!args.containsKey(SHOP_ITEM_ID)) {
+            throw RuntimeException("Param shop item id is absent")
+        }
+        shopItemId = args.getInt(SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+    }
+
+    private fun setupResetErrorInputName() {
+        binding.etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    private fun setupResetErrorInputCount() {
+        binding.etCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputCount()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    companion object {
+        private const val SHOP_ITEM_ID = "id"
+
+        fun newInstance(id: Int): ShopItemFragment {
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(SHOP_ITEM_ID, id)
+                }
+            }
+        }
+    }
+}
